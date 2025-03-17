a0878b6236f2d9867652042a8cc00f0f60155391
BrandGerry <91706030+BrandGerry@users.noreply.github.com>
2025-03-16 23:30:03 -0600

    ADD:REDUX TOOLKIT

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./styles.css";
-import CalendarApp from "./CalendarApp.jsx";
+import { CalendarApp } from "./CalendarApp";
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
   <>
